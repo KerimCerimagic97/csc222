1bcdbf8bf405df5269066dc5b4390b78be14807c
Kerim Cerimagic <kcerimagic@hotmail.com>
2026-09-11 09:12:00 -0400

    Update c3_exercise0.cpp

--- practice/c3_exercise0.cpp
+++ practice/c3_exercise0.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
 
+//Question 1:
+
 void new_line(){
     cout << endl;
 }
@@ -11,6 +13,24 @@ void adding_new_func(){
   cout << "Second Line." << endl;
 }
 
+//if new line is declared after adding new func, it doesn't work
+
+//Question 2:
+
+void print_twice(char phil) {
+    cout << phil << phil << endl;
+}
+
+void par_arg(){
+    print_twice('a');
+    print_twice('b');
+    print_twice('c');
+    print_twice(24);
+    print_twice(24.6);
+    print_twice("tryme");
+}
+
+//Question 3:
 
 int main()
 {
