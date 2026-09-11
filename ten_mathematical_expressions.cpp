64c4af991147b9b87d7bb93be810bca9627b5272
Kerim Cerimagic <kcerimagic@hotmail.com>
2026-09-11 04:17:21 -0400

    Create ten_mathematical_expressions.cpp

---
+++ ten_mathematical_expressions.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int three = 3;
+  int four = 4;
+  int five = 5;
+  int six = 6;
+  int seven = 7;
+  int eight = 8;
+
+  cout << "The sum of " << three <<" and " << seven << " equals " << three+seven << "." << endl;
+
+  cout << "The difference of " << eight <<" and " << seven << " equals " << eight-seven << "." << endl;
+
+  cout << "The product of " << three <<" and " << five << " equals " << three*five << "." << endl;
+
+  cout << "The sum of " << five <<" and " << six << " equals " << five + six << "." << endl;
+
+  cout << "The quotient of " << six <<" and " << three << " equals " << six/three << "." << endl;
+
+  cout << "The product of " << three <<" and " << three << " equals " << three*three << "." << endl;
+
+  cout << "The remainder of " << seven <<" and " << two << " equals " << seven%two << "." << endl;
+
+  cout << "The sum of " << six <<" and " << seven << " equals " << six+seven << "." << endl;
+
+  cout << "The difference of " << seven <<" and " << three << " equals " << seven-three << "." << endl;
+
+  cout << "The quotient of " << eight <<" and " << four << " equals " << eight/four << "." << endl;
+
+  cout << "The sum of " << eight <<" and " << seven << " equals " << eight+seven << "." << endl;
+
+  return 0;
+}
